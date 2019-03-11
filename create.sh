bbd80ba4b6826f9b6cc4a12a310234e5903622f7
danielrmartin <danielraymartin@gmail.com>
2019-03-11 13:14:49 -0500

    adding code for managing CloudBees

--- create.sh
+++ create.sh
@@ -1,8 +1,8 @@
-!# /bin/bash
-set DOMAIN=cje.dmartin-cje.com
-set cluster-name=dmartin-cje
-gcloud container clusters create $cluster-name --zone us-central1-a,us-central1-b --machine-type=n1-standard-8 --num-nodes=2
+#!/usr/bin/env bash
+DOMAIN=cje.dmartin-cje.com
+name=dmartin-cje
+gcloud container clusters create $name --region us-central1 --machine-type=n1-standard-2 --num-nodes=2
 kubectl create clusterrolebinding cluster-admin-binding  --clusterrole cluster-admin  --user $(gcloud config get-value account)
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-gcloud beta compute ssl-certificates create $cluster-name --domains $DOMAIN 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+#gcloud beta compute ssl-certificates create $name --domains $DOMAIN 
