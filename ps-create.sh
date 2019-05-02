ee1a18d29a4e378882c9fabdf60a5f212392e42e
danielrmartin <danielraymartin@gmail.com>
2019-05-02 08:52:53 -0500

    Updating new scripts and cheatsheets

--- ps-create.sh
+++ ps-create.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 DOMAIN=cje.dmartin-cje.com
-name=dmartin-cje
+name=cb-ps-team
 region=us-central1
-gcloud container clusters create $name --region=$region  --machine-type=n1-standard-4 --num-nodes=2  --enable-autoscaling --min-nodes 1 --max-nodes 4
+gcloud container clusters create $name --region=$region  --machine-type=n1-standard-4 --num-nodes=2  --enable-autoscaling --min-nodes 1 --max-nodes 4 --enable-basic-auth
 kubectl create clusterrolebinding cluster-admin-binding  --clusterrole cluster-admin  --user $(gcloud config get-value account)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
