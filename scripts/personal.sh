44f483d502f6e261c49ee4cd3ef19ec45616270f
Daniel Martin <Daniel@macbook-pro.lan>
2019-09-26 15:26:33 -0500

    updating partner work

--- scripts/personal.sh
+++ scripts/personal.sh
@@ -2,17 +2,19 @@
 DOMAIN=cje.dmartin-cje.com
 name=partners-cje
 region=us-central1
-gcloud container clusters create $name --region=$region  --machine-type=n1-standard-2 --num-nodes=1  --enable-autoscaling --min-nodes 1 --max-nodes 1
+gcloud container clusters create $name --region=$region  --machine-type=n1-standard-2 --num-nodes=1  --enable-autoscaling --min-nodes 1 --max-nodes 1 --enable-basic-auth
 kubectl create clusterrolebinding cluster-admin-binding  --clusterrole cluster-admin  --user $(gcloud config get-value account)
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl apply -f ../yaml_files/ssd-storage.yaml
+#kubectl apply -f ../yaml_files/ssd-storage.yaml
 helm init --service-account tiller
 kubectl apply -f ../yaml_files/rbac-config.yaml
+./change-storage.sh
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
 #gcloud beta compute ssl-certificates create $name --domains $DOMAIN
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+sleep 3m
 helm install --name cert-manager --namespace kube-system --version v0.6.0 stable/cert-manager --set webhook.enabled=false 
 #gcloud iam service-accounts create clouddns --display-name=clouddns --project cje20-200714
 gcloud iam service-accounts keys create ./clouddns.key.json --iam-account=clouddns@cje20-200714.iam.gserviceaccount.com --project=cje20-200714
